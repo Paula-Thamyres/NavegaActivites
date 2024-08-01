3e6bab682dcadd098eb7274a5a0b0647ace3e6f3
Paula <paula.arthamys@gmail.com>
2024-08-01 18:49:10 -0300

    Initial commit

---
+++ app/src/main/java/com/paula/navegaactivites/SecondActivity.java
@@ -0,0 +1,41 @@
+package com.paula.navegaactivites;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Objects;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private Button btnThirdActivity;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_second);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        btnThirdActivity = findViewById(R.id.btn_third_activity);
+
+        btnThirdActivity.setOnClickListener(view -> {
+            startActivity(new Intent(this, ThirdActivity.class));
+        });
+
+    }
+}
